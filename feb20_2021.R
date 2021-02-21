1ea165aa3f3b2e03e4d763f610d699aa97fcd3c6
Keun Young Ko <keunyoungko@hotmail.com>
2021-02-20 21:13:42 -0800

    adding tuning parameters and finding accuracy rates

--- feb20_2021.R
+++ feb20_2021.R
@@ -41,7 +41,9 @@ library(ISLR)
 library(xtable)
 library(MASS)
 library(pastecs)
-
+library(extraTrees)
+library(caretEnsemble)
+library(class)
 # Reading files
 training.data <- read.csv("TrainingWiDS2021.csv")
 
@@ -93,23 +95,85 @@ training.data.spec <- training.data.spec %>%
   drop_na() 
 exclude_cols <- c("X", "encounter_id", "hospital_id", "icu_id")
 training.data.spec2 = training.data.spec[!names(training.data.spec) %in% exclude_cols]
+
 ### TRAINING & TESTING ###
-model <- train(factor(diabetes_mellitus) ~ ., 
-               data = training.data.spec,
-               method = "ranger",
-               tuneLength = 5) 
-var
-# Random Forest
-
-rf <- randomForest(factor(diabetes_mellitus) ~ .,
-                   data = training.data.spec2,
-                   ntree=1000, keep.forest=FALSE,
-                   importance=TRUE)
-varImpPlot(rf)
 # Cross-validation
-
 indexes <- sample(nrow(training.data.spec),
                   size = nrow(training.data.spec) * 0.7)
 
 train.subset <- training.data.spec[indexes, ] #grabbing rows from the indexes
 test.subset <- training.data.spec[-indexes, ]
+
+model.trial <- train(factor(diabetes_mellitus) ~ ., 
+               data = training.data.spec2,
+               method = "ranger", #ranger for randomforest
+               tuneLength = 5) 
+model$bestTune 
+plot(model) #extratrees renders on average higher accuracy (boostrap) than gini. the smaller the number of randomly selected predictors, the more accurate the prediction.
+
+tune.grid <- expand.grid(mtry = 1:2,
+                         splitrule = c("gini", "extratrees"),
+                         min.node.size = 1)
+
+model2 <- train(factor(diabetes_mellitus) ~ d1_glucose_max + glucose_apache + d1_glucose_min + bmi + weight + age + d1_creatinine_max + d1_creatinine_min + creatinine_apache,
+                data = training.data.spec2,
+                method = "ranger",
+                tuneGrid = tune.grid)
+model2
+
+## Ensemble
+algorithmList <- c("rf", "rpart", "knn", "lda") 
+model3 <- caretList(factor(diabetes_mellitus) ~ d1_glucose_max + glucose_apache + d1_glucose_min + bmi + weight + age + d1_creatinine_max + d1_creatinine_min + creatinine_apache,
+                    data = training.data.spec2,
+                    methodList = algorithmList,
+                    trControl = trainControl)
+
+# basic parameter tuning to control for cross-validation // resampling
+cv5 <- trainControl(method = "repeatedcv",
+                    number = 5,
+                    repeats = 5)
+model4 <- train(factor(diabetes_mellitus) ~ d1_glucose_max + glucose_apache + d1_glucose_min + bmi + weight + age + d1_creatinine_max + d1_creatinine_min + creatinine_apache,
+                data = training.data.spec2,
+                method = "ranger",
+                tuneGrid = data.frame(mtry = 2,
+                                      min.node.size = 1,
+                                      splitrule = "extratrees"),
+                trControl = cv5)
+model4 # accuracy = 0.8124
+
+## Random Forest ##
+# Trial 1
+rf <- randomForest(factor(diabetes_mellitus) ~ .,
+                   data = training.data.spec2,
+                   ntree=1000, keep.forest=FALSE,
+                   importance=TRUE)
+varImpPlot(rf)
+# Key variables in random forest include:
+# d1_glucose_max, glucose_apache, d1_glucose_min, bmi, weight, age, d1_creatinine_max, d1_creatinine_min, and creatinine_apache
+
+# Trial 2
+ind <- sample(2, nrow(training.data.spec2), replace = TRUE, prob=c(0.8, 0.2))
+rf.true.vals <- training.data.spec2[ind == 2,]$diabetes_mellitus
+rf.tree.train <- randomForest(factor(diabetes_mellitus) ~ d1_glucose_max + glucose_apache + d1_glucose_min + bmi + weight + age + d1_creatinine_max + d1_creatinine_min + creatinine_apache,
+                        data=training.data.spec2[ind == 1,]) #train data
+rf.tree.preds <- predict(rf.tree.train, training.data.spec2[ind == 2,]) #test data
+table(observed = training.data.spec2[ind==2, "diabetes_mellitus"], predicted = rf.tree.preds)
+## Get prediction for all trees
+rf.tree.preds <- predict(rf.tree, training.data.spec2[ind == 2,], predict.all=TRUE)
+mean(rf.tree.preds == rf.true.vals) # accuracy = 0.815827
+
+# Bagging
+true.vals <- test.subset$diabetes_mellitus
+bagging.model.train <- bagging(factor(diabetes_mellitus) ~ d1_glucose_max + glucose_apache + d1_glucose_min + bmi + weight + age + d1_creatinine_max + d1_creatinine_min + creatinine_apache,
+                               data = train.subset)
+bagging.preds <- predict(bagging.model.train, test.subset)
+mean(bagging.preds == true.vals) # accuracy = 0.7992341
+
+# rpart
+rpart.tree.model.train <- rpart(factor(diabetes_mellitus) ~ d1_glucose_max + glucose_apache + d1_glucose_min + bmi + weight + age + d1_creatinine_max + d1_creatinine_min + creatinine_apache,
+                          data = train.subset)
+rpart.tree.preds <- ifelse(predict(tree.model.train, test.subset)[,2] > 0.5, 1, 0)
+
+mean(bagging.preds == true.vals)
+mean(tree.preds == true.vals) # accuracy = 0.8122265
+
